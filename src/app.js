dc090605381718a3b6aa989a3c0ae5002f03344c
balintkiraly <balint.kiraly@protonmail.com>
2020-09-24 20:25:00 +0200

    Init express app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+import routes from './routes'
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './', 'views'))
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
