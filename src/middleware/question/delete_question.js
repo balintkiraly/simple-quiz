0abde004bc1a402f2dacc3d83564cf5e72b9c22a
balintkiraly <balint.kiraly@protonmail.com>
2020-11-09 21:14:46 +0100

    Fix some typo

--- src/middleware/question/delete_question.js
+++ src/middleware/question/delete_question.js
@@ -1,7 +1,7 @@
 import { Question } from '../../models'
 
 export const deleteQuestion = (req, _res, next) => {
-  await question.deleteOne({ id: req.params.id })
+  await Question.deleteOne({ id: req.params.id })
 
   next()
 }
