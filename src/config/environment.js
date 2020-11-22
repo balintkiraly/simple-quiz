8ae582b2ac6709aeddf7a2925d96641ef66d57d7
balintkiraly <balint.kiraly@protonmail.com>
2020-11-22 17:21:00 +0100

    Add reset password

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,3 +3,13 @@ require('dotenv').config()
 export const MONGODB_URL =
   process.env.MONGODB_URL || 'mongodb://mongoadmin:secret@localhost:27017/db'
 export const APP_PORT = process.env.APP_PORT || 3000
+
+export const emailOptions = {
+  service: process.env.EMAIL_SERVICE || 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER || 'youremail@gmail.com',
+    pass: process.env.EMAIL_PASSWORD || 'yourpassword',
+  },
+}
+
+export const url = (path) => `${process.env.APP_URL}${path}`
