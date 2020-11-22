01e9b26f9484ad9ebbfc7cad9691a13dd5ab7819
balintkiraly <balint.kiraly@protonmail.com>
2020-11-22 13:47:47 +0100

    Some refactor

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
-import { User } from '../models'
 import { login, logout, resetPassword } from '../middleware/auth'
 import { createUser } from '../middleware/user'
-import { validate } from '../utils/validator'
+import { validate } from '../utils'
+import { registrationValidator } from '../validators'
 
 const router = Router()
 
@@ -23,34 +22,7 @@ router.post('/login', login)
 router.post('/logout', logout)
 router.post(
   '/registration',
-  validate(
-    [
-      body('name').isString().notEmpty(),
-      body('email')
-        .isEmail()
-        .custom((value) => {
-          return new Promise((resolve, reject) => {
-            User.findOne({ email: value }, function (err, user) {
-              if (err) {
-                reject(new Error('Server Error'))
-              }
-              if (Boolean(user)) {
-                reject(new Error('E-mail already in use'))
-              }
-              resolve(true)
-            })
-          })
-        }),
-      body('password').isLength({ min: 6 }),
-      body('passwordConfirmation').custom((value, { req }) => {
-        if (value != req.body.password) {
-          throw new Error('Password confirmation does not match password')
-        }
-        return true
-      }),
-    ],
-    'auth/registration',
-  ),
+  validate(registrationValidator, 'auth/registration'),
   createUser,
 )
 router.post('/reset_password', resetPassword)
