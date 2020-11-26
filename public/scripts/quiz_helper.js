394c54f21ee66ceceae0e3fa8324254d4243cca1
balintkiraly <balint.kiraly@protonmail.com>
2020-11-26 22:09:09 +0100

    Add quiz helper

---
+++ public/scripts/quiz_helper.js
@@ -0,0 +1,37 @@
+const createQuestion = async (quizId, body) => {
+  const response = await fetch(`/quiz/${quizId}/questions`, {
+    method: 'POST',
+    body,
+  })
+  return response.json()
+}
+
+const updateQuestion = async (quizId, questionId, body) => {
+  const response = await fetch(`/quiz/${quizId}/questions/${questionId}`, {
+    method: 'PUT',
+    body,
+  })
+  return response.json()
+}
+
+const deleteQuestion = async (quizId, questionId) => {
+  const response = await fetch(`/quiz/${quizId}/questions/${questionId}`, {
+    method: 'DELETE',
+  })
+  return response.json()
+}
+
+const updateQuiz = async (quizId, body) => {
+  const response = await fetch(`/quiz/${quizId}`, {
+    method: 'PUT',
+    body,
+  })
+  return response.json()
+}
+
+const deleteQuiz = async (quizId) => {
+  const response = await fetch(`/quiz/${quizId}`, {
+    method: 'DELETE',
+  })
+  return response.json()
+}
