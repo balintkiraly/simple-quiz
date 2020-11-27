412ffd77e680fed5230d571c91c178cb7458db86
balintkiraly <balint.kiraly@protonmail.com>
2020-11-27 16:36:17 +0100

    Add fill quiz

--- src/middleware/quiz/send_quiz.js
+++ src/middleware/quiz/send_quiz.js
@@ -1,4 +1,18 @@
-export const sendQuiz = (_req, _res, next) => {
-  // TODO: Calculate the score based by the answers
-  next()
+import { Quiz } from '../../models'
+
+export const sendQuiz = async (req, res, next) => {
+  const quiz = await Quiz.findById(req.params.id).populate('questions')
+
+  let correct = 0
+  quiz.questions.forEach((question) => {
+    if (question.correctAnswer == req.body[`answer-${question.id}`]) {
+      correct++
+    }
+  })
+
+  req.quiz = quiz
+  req.score = Math.round((correct / quiz.questions.length) * 100)
+
+  // TODO: Save if logged in
+  return next()
 }
--- src/routes/quiz.routes.js
+++ src/routes/quiz.routes.js
@@ -12,6 +12,7 @@ import {
   getQuizzes,
   getQuiz,
   isOwnQuiz,
+  sendQuiz,
 } from '../middleware/quiz'
 import { isAuthenticated } from '../middleware/auth'
 
@@ -37,6 +38,9 @@ router.get('/:id/edit', isAuthenticated, isOwnQuiz, getQuiz, (req, res) => {
   res.render('quiz/edit', { quiz: req.quiz })
 })
 
+router.post('/:id/submit', sendQuiz, (req, res) => {
+  res.render('quiz/success', { score: req.score, quiz: req.quiz })
+})
 router.post('/:id/questions', isAuthenticated, isOwnQuiz, createQuestion)
 router.put('/:id/questions/:qid', isAuthenticated, isOwnQuiz, updateQuestion)
 router.delete('/:id/questions/:qid', isAuthenticated, isOwnQuiz, deleteQuestion)
