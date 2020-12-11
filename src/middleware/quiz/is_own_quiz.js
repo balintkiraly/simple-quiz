bba2103c95f4aa2265a29a5284ea101069cbd07e
balintkiraly <balint.kiraly@protonmail.com>
2020-12-11 21:58:04 +0100

    Add first test

--- src/middleware/quiz/is_own_quiz.js
+++ src/middleware/quiz/is_own_quiz.js
@@ -3,11 +3,11 @@ import { getUserID } from '../../utils'
 
 export const isOwnQuiz = async (req, res, next) => {
   const quiz = await Quiz.find({
-    id: req.params.id,
+    _id: req.params.id,
     owner: getUserID(req.session),
   })
 
-  if (quiz) {
+  if (quiz.length) {
     return next()
   } else {
     res.render('errors/401')
